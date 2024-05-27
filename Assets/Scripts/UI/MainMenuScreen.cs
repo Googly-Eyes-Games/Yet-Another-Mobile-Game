24b2998ae2149f9b506e0fc563674d5ca003e9a9
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2024-05-27 17:15:31 +0200

    feat: Mute audio

--- Assets/Scripts/UI/MainMenuScreen.cs
+++ Assets/Scripts/UI/MainMenuScreen.cs
@@ -2,23 +2,44 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenuScreen : MonoBehaviour
 {
     [SerializeField] private IntSOEvent onScoreChanged;
+    [SerializeField] private Button muteButton;
 
     private void OnEnable()
     {
         onScoreChanged.OnRaise += OnScoreChanged;
+        muteButton.onClick.AddListener(OnMutePressed);
     }
 
     private void OnDisable()
     {
         onScoreChanged.OnRaise -= OnScoreChanged;
+        muteButton.onClick.RemoveListener(OnMutePressed);
     }
 
     private void OnScoreChanged(int obj)
     {
         gameObject.SetActive(false);
+    }
+
+    private void OnMutePressed()
+    {
+        bool isMuted = AudioListener.volume == 0.0f;
+        AudioListener.volume = isMuted ? 1.0f : 0.0f;
+    }
+
+    private void OnShopPressed()
+    {
+        //TODO
+    }
+
+    private void OnHelpPressed()
+    {
+        //TODO
     }
 }
