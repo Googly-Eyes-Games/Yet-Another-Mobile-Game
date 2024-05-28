a107e55443b0a69669faa2f7d8bfe82c7835081d
Mateusz Banasik <kilifugame@gmail.com>
2024-05-28 17:31:32 +0200

    Add basic shop with ShopItemSO

---
+++ Assets/Scripts/Shop/ItemTemplate.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemTemplate : MonoBehaviour
+{
+    public TextMeshProUGUI titleText;
+    public Image image;
+    public TextMeshProUGUI priceText;
+
+}
--- Assets/Scripts/UI/Popup/ShopMenu.cs
+++ Assets/Scripts/UI/Popup/ShopMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,30 @@ using UnityEngine.UI;
 
 public class ShopMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject shopContent;
+    [SerializeField] private GameObject itemTemplate;
+    [SerializeField] private List<ShopItemSO> ShopItemsSO;
+
+
+    private void Awake()
+    {
+        foreach (var shopItemSO in ShopItemsSO)
+        {
+            GameObject newItem = Instantiate(itemTemplate);
+            
+            itemTemplate.GetComponent<ItemTemplate>().titleText.text = shopItemSO.Title;
+            itemTemplate.GetComponent<ItemTemplate>().image.sprite = shopItemSO.Sprite;
+            itemTemplate.GetComponent<ItemTemplate>().priceText.text = shopItemSO.Price.ToString();
+            
+            newItem.transform.SetParent(shopContent.transform, false);
+
+        }
+    }
+
     private void OnEnable() 
     {
-
+        
+        
     }
 
     private void OnDisable()
