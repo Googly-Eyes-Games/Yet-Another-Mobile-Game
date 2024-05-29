75a871de4ecfcf3e289041db8ef2da50ef935c4c
Mateusz Banasik <kilifugame@gmail.com>
2024-05-29 13:27:17 +0200

    Add possibility to buy line

--- Assets/Scripts/UI/Popup/ShopMenu.cs
+++ Assets/Scripts/UI/Popup/ShopMenu.cs
@@ -2,36 +2,57 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class ShopMenu : MonoBehaviour
 {
     [SerializeField] private GameObject Player;
-    [SerializeField] private GameObject shopContent;
     [SerializeField] private GameObject itemTemplate;
-    [SerializeField] private List<ShopItemSO> ShopItemsSO;
+    
+    [SerializeField] private GameObject boatsContent;
+    [SerializeField] private GameObject linesContent;
+    
+    [SerializeField] private List<ShopItemSO> boatItemsSO;
+    [SerializeField] private List<ShopItemSO> lineItemsSO;
 
     private void Start()
     {
-        foreach (var shopItemSO in ShopItemsSO)
+        foreach (var boatItemSO in boatItemsSO)
         {
-            GameObject newItem = Instantiate(itemTemplate, shopContent.transform, false);
-
-            ItemTemplate itemTemplateComponent = newItem.GetComponent<ItemTemplate>();
-            
-            itemTemplateComponent.titleText.text = shopItemSO.Title;
-            itemTemplateComponent.image.sprite = shopItemSO.Sprite;
-            itemTemplateComponent.image.color = shopItemSO.Color;
-            itemTemplateComponent.priceText.text = shopItemSO.Price.ToString();
-            
-            itemTemplateComponent.button.onClick.AddListener( () => 
+            CreateItem(boatItemSO, boatsContent.transform, () => 
             {
-                Player.GetComponentInChildren<SpriteRenderer>().color = shopItemSO.Color;
+                Player.GetComponentInChildren<SpriteRenderer>().color = boatItemSO.Color;
+            });
+        }
+
+        foreach (var lineItemSO in lineItemsSO)
+        {
+            CreateItem(lineItemSO, linesContent.transform, () =>
+            {
+                var lineRenderer = Player.GetComponentInChildren<LineRenderer>();
+                lineRenderer.startColor = lineItemSO.Color;
+                lineRenderer.endColor = lineItemSO.Color;
             });
         }
     }
-    
 
+    private void CreateItem(ShopItemSO itemSO, Transform parentTransform, UnityEngine.Events.UnityAction onClickAction)
+    {
+        GameObject newItem = Instantiate(itemTemplate, parentTransform, false);
+    
+        ItemTemplate itemTemplateComponent = newItem.GetComponent<ItemTemplate>();
+    
+        itemTemplateComponent.titleText.text = itemSO.Title;
+        itemTemplateComponent.image.sprite = itemSO.Sprite;
+        itemTemplateComponent.image.color = itemSO.Color;
+        itemTemplateComponent.priceText.text = itemSO.Price.ToString();
+    
+        itemTemplateComponent.button.onClick.AddListener(onClickAction);
+    }
+    
+    
+    
     private void OnEnable() 
     {
         
