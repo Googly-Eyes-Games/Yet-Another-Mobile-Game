1c86658bed69910e9dc236e8a064ff45740bb16a
Mateusz Banasik <kilifugame@gmail.com>
2024-06-11 01:05:23 +0200

    Add some buying logic in shop

--- Assets/Scripts/UI/Popup/ShopMenu.cs
+++ Assets/Scripts/UI/Popup/ShopMenu.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -16,13 +18,33 @@ public class ShopMenu : MonoBehaviour
     [SerializeField] private List<ShopItemSO> boatItemsSO;
     [SerializeField] private List<ShopItemSO> lineItemsSO;
 
-    private void Start()
+    private Dictionary<Button, ShopItemSO> buttonsDict = new();
+    private Dictionary<ShopItemSO, bool> itemsDict = new();
+
+    private void Awake()
     {
+        GameSave newSave = SaveManager.Instance.Save;
+        
         foreach (var boatItemSO in boatItemsSO)
         {
-            CreateItem(boatItemSO, boatsContent.transform, () => 
+            CreateItem(boatItemSO, boatsContent.transform, () =>
             {
-                Player.GetComponentInChildren<SpriteRenderer>().sprite = boatItemSO.Sprite;
+                Button button = buttonsDict.FirstOrDefault(x => x.Value == boatItemSO).Key;
+                if (button == null)
+                    return;
+
+                if (itemsDict[boatItemSO])
+                {
+                    button.interactable = false;
+                    button.GetComponentInChildren<TextMeshProUGUI>().text = "In use";
+                    Player.GetComponentInChildren<SpriteRenderer>().sprite = boatItemSO.Sprite;
+                    return;
+                }
+
+                newSave.MoneyAmount -= boatItemSO.Price;
+                button.GetComponentInChildren<TextMeshProUGUI>().text = "Use";
+                itemsDict[boatItemSO] = true;
+
             });
         }
 
@@ -36,26 +58,39 @@ public class ShopMenu : MonoBehaviour
             });
         }
     }
-
+    
     private void CreateItem(ShopItemSO itemSO, Transform parentTransform, UnityEngine.Events.UnityAction onClickAction)
     {
         GameObject newItem = Instantiate(itemTemplate, parentTransform, false);
     
         ItemTemplate itemTemplateComponent = newItem.GetComponent<ItemTemplate>();
-    
+        
         itemTemplateComponent.titleText.text = itemSO.Title;
         itemTemplateComponent.image.sprite = itemSO.Sprite;
         itemTemplateComponent.image.color = itemSO.Color;
         itemTemplateComponent.priceText.text = itemSO.Price.ToString();
     
         itemTemplateComponent.button.onClick.AddListener(onClickAction);
+        buttonsDict.Add(itemTemplateComponent.button, itemSO);
+        itemsDict.Add(itemSO, itemSO.Purchased);
+
+        if (!itemSO.Purchased) 
+            return;
+        
+        itemTemplateComponent.button.interactable = false;
+        itemTemplateComponent.button.GetComponentInChildren<TextMeshProUGUI>().text = "In use";
+        
     }
-    
     
     
     private void OnEnable() 
     {
+        GameSave newSave = SaveManager.Instance.Save;
         
+        foreach (var button in buttonsDict.Keys)
+        {
+            button.interactable = buttonsDict[button].Price <= newSave.MoneyAmount;
+        }
         
     }
 
