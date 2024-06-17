ba61fca7623eec0d4fb2ae386d58355b56f35a4b
Mateusz Banasik <kilifugame@gmail.com>
2024-06-17 19:50:09 +0200

    Add items button changing color

---
+++ Assets/Scripts/Shop/ColorButtonManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorButtonManager : MonoBehaviour
+{
+    public Color interactableButtonColor;
+    public Color nonInteractableButtonColor;
+
+    public Color interactableTextColor;
+    public Color nonInteractableTextColor;
+
+    public void ChangeButtonAppearance(Button button, bool interactable)
+    {
+        Image buttonImage = button.gameObject.GetComponent<Image>();
+        TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+        
+        if (!buttonImage && !buttonText)
+            return;
+        
+        buttonImage.color = interactable ? interactableButtonColor : nonInteractableButtonColor;
+        buttonText.color = interactable ? interactableTextColor : nonInteractableTextColor;
+    }
+}
+    
+
