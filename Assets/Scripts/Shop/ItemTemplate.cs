72bab77caf66fdd7521bab75aee1acc9605bce70
Mateusz Banasik <kilifugame@gmail.com>
2024-06-18 20:55:17 +0200

    Change colors of popups

--- Assets/Scripts/Shop/ItemTemplate.cs
+++ Assets/Scripts/Shop/ItemTemplate.cs
@@ -4,9 +4,6 @@ using UnityEngine.UI;
 
 public class ItemTemplate : MonoBehaviour
 {
-    public Color defaultTextColor;
-    public Color disabledTextColor;
-    
     public TextMeshProUGUI titleText;
     public Image image;
     public TextMeshProUGUI priceText;
